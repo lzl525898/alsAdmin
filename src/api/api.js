e4951bf70f6fcc53ae166c2aec8eb45b0d797491
lzl525898 <luckyforlei@163.com>
2020-01-02 23:07:59 +0800

    添加header

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,19 @@
-import ajax from './ajax'
-export const reqLogin = (username, password)=> ajax('/login',{username,password},'POST')
+import jsopp from 'jsonp';
+import ajax from './ajax';
+import { message } from 'antd';
+// 用户登录
+export const reqLogin = (username, password)=> ajax('/login',{username,password},'POST');
+// 获取天气
+export const reqWeather = (city) => {
+    return new Promise((resolve, reject) => {
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`;
+        jsopp(url,{},(err,data) => {
+            if(!err && data.status==='success'){
+                const {dayPictureUrl, weather} = data.results[0].weather_data[0];
+                resolve({dayPictureUrl, weather});
+            }else{
+                message.error('获取天气失败');
+            }
+        })
+    })
+}
